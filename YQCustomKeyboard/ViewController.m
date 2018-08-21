414310a2b830751128fa253b198791dbb2bd0093
杨清 <yangqing1989@icloud.com>
2018-08-21 14:41:58 +0800

    modify intro

--- YQCustomKeyboard/ViewController.m
+++ YQCustomKeyboard/ViewController.m
@@ -3,7 +3,7 @@
 //  YQCustomKeyboard
 //
 //  Created by 杨清 on 2018/8/8.
-//  Copyright © 2018年 深圳市前海硕极科技有限公司. All rights reserved.
+//  Copyright © 2018年 Soargift. All rights reserved.
 //
 
 #import "ViewController.h"
